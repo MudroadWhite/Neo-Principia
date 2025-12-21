bead97122af75813472bbaf0c7281aac5c56ec44
MudroadWhite <53061787+MudroadWhite@users.noreply.github.com>
2025-12-21 20:25:19 +0800

    minor: draft

--- pm/ch14.v
+++ pm/ch14.v
@@ -9,19 +9,19 @@ Require Import PM.pm.ch11.
 Require Import PM.pm.ch12.
 Require Import PM.pm.ch13.
 
-(* TODO: polish the note: the definition of description in text is left with ambiguity:
-since the original definition is defined in a pure substitutional way without 
-any considerations for scopes, there has an attempt being made to set up a 
-naive considerations on how the scope should be resolved 
+(* TODO: polish the note: the definition of description in text is left with ambiguity with regard to 
+the dot notation so we add a scope operator to patch the notation
 
-How does the notation work
+scope operator should be considered at the same level as dot notation, but it has been actually written 
+down into definition
+
+How does the description work
 1. original idea: [scope] ( *the expression containing the iota operator* )
 2. since scope is usually the minimal expression containing the iota, we often omit the [scope] part
 3. finally we add corner cases: definitions for scope specification for multiple iotas, and case 
   when `a != x` is necessary to be concerned
 
-We want to ignore the scope notation since it's a patch to the dot notation, while we can 
-resolve this easily with brackets
+We won't treat the scope operator seriously since we can resolve this easily with brackets in rocq
 *)
 
 (* `_f` suffix means it's for typical (untyped) functions *)
@@ -32,3 +32,5 @@ Definition iota_f (Phi Psi : Prop -> Prop) :=
   something like `a != x`. This seems unsatisfying because we might be unable to determine which
   definition to use *)
 Definition iota_p (E : Prop) (Phi : Prop -> Prop) := exists b, (Phi x <[- x -]> (x = b)).
+
+Definition scope_iota_f. Admitted.
