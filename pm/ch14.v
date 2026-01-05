f4e9326aa84aa25889db3af5e1096d5e4b531563
MudroadWhite <53061787+MudroadWhite@users.noreply.github.com>
2026-01-05 21:58:16 +0800

    minor: update

--- pm/ch14.v
+++ pm/ch14.v
@@ -11,20 +11,8 @@ Require Import PM.pm.ch13.
 
 Require Import Logic.FunctionalExtensionality.
 
-(* TODO: polish the note: the definition of description in text is left with ambiguity with regard to 
-the dot notation so we add a scope operator to patch the notation
+(* TODO: How does the iota description work-
 
-scope operator should be considered at the same level as dot notation, but it has been actually written 
-down into definition
-
-How does the description work
-1. original idea: [scope] ( *the expression containing the iota operator* )
-2. since scope is usually the minimal expression containing the iota, we often omit the [scope] part
-3. finally we add corner cases: definitions for scope specification for multiple iotas, and case 
-  when `a != x` is necessary to be concerned
-
-My plan: in the future we might want to utilize currying to define some combinators and see if can be used
-as an alternative.....
 *)
 
 (* TODO: make the definitions into a notation in the future *)
