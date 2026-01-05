f0f1f757f7763c0f8e8806fd5fb9141f258b98bd
MudroadWhite <53061787+MudroadWhite@users.noreply.github.com>
2026-01-05 11:01:48 +0800

    minor: progress in *14.111

--- pm/ch14.v
+++ pm/ch14.v
@@ -30,7 +30,6 @@ as an alternative.....
 
 Open Scope single_app_equiv.
 
-(* Definition Iota (s : string) (x : Prop) : Type := Prop. *)
 Definition Iota (s : string) (x : Prop) : Prop := x.
 
 Example iota_function (i1 i2 : Prop) : Prop -> Prop :=
@@ -67,14 +66,18 @@ Definition iota_f2
       (fun c => f (Iota s1 b) (Iota s2 c))).
 
 (* cf. p174, explanation after *14.04. The iota variant where inner function has 
-  larger scope than outer function. This variant will be proven later unecessary. *)
+  larger scope than outer function. This variant will be proven later unecessary. 
+
+  The original definition depends on `iota_f2`. The function `iota_f` here, 
+  provided with parameters, gets a similar role to the idea of scope
+*)
 Definition iota_f2_1 
-  (s2 s1 : string)
-  (Psi Phi : Prop -> Prop)
+  (s1 s2 : string)
+  (Phi Psi : Prop -> Prop)
   (f : Prop -> Prop -> Prop) :=
-  iota_f2 s2 s1 Psi Phi.
-  (* exists b, (Phi x <[- x -]> (x = b)) /\ 
-    ((exists c, Psi x <[- x -]> (x = c) /\ f b c)). *)
+  iota_f s2 Psi
+    (fun c => iota_f s1 Phi
+      (fun b => f (Iota s1 b) (Iota s2 c))).
 
 (* `_p` suffix means it's for predicates. 
   This definition could have several meanings(?to be checked):
@@ -89,8 +92,8 @@ Definition iota_f2_1
 *)
 Definition iota_p (E : Predicate 1) (Phi : Prop -> Prop) := exists b, (Phi x <[- x -]> (x = b)).
 
-Definition n14_01 (s1 : string) (Phi Psi : Prop -> Prop) : 
-  (iota_f s1 Phi Psi) = exists b, (Phi x <[- x -]> (x = b)) /\ Psi b. 
+Definition n14_01 (s : string) (Phi Psi : Prop -> Prop) : 
+  (iota_f s Phi Psi) = exists b, (Phi x <[- x -]> (x = b)) /\ Psi b. 
 Admitted.
 
 (* TODO: check if iota_p and *14.02 is correctly defined *)
@@ -103,24 +106,26 @@ Definition n14_03 (s1 s2 : string) (Phi Psi : Prop -> Prop) (f : Prop -> Prop ->
     /\ ((exists c, Psi x <[- x -]> (x = c) /\ f b c)).
 Admitted.
 
-Definition n14_04 (s2 s1 : string) (Psi Phi : Prop -> Prop) (f : Prop -> Prop -> Prop) : 
-  (iota_f2_1 s2 s1 Psi Phi f) = exists b, (Phi x <[- x -]> (x = b)) 
-    /\ ((exists c, Psi x <[- x -]> (x = c) /\ f b c)).
+Definition n14_04 (s1 s2 : string) (Phi Psi : Prop -> Prop) (f : Prop -> Prop -> Prop) : 
+  (iota_f2_1 s2 s1 Psi Phi f) = 
+  iota_f2 s2 s1 Psi Phi (fun x y => f y x).
+  (* exists b, (Phi x <[- x -]> (x = b)) 
+    /\ ((exists c, Psi x <[- x -]> (x = c) /\ f b c)). *)
 Admitted.
 
-Theorem n14_1 (Phi Psi : Prop -> Prop) : (iota_f Phi Psi) <-> 
+Theorem n14_1 (Phi Psi : Prop -> Prop) (s : string) : (iota_f s Phi Psi) <-> 
   exists b, (Phi x <[- x -]> (x = b)) /\ Psi b.
 Proof.
-  pose proof (n4_2 (iota_f Phi Psi)) as n4_2.
+  pose proof (n4_2 (iota_f s Phi Psi)) as n4_2.
   now rewrite -> n14_01 in n4_2 at 2.
 Qed.
 
 (* The equivalent with n14_1, with scope notation in its original 
   representation omitted  *)
-Theorem n14_101 (Phi Psi : Prop -> Prop) : (iota_f Phi Psi) <-> 
+Theorem n14_101 (Phi Psi : Prop -> Prop) (s : string) : (iota_f s Phi Psi) <-> 
   exists b, (Phi x <[- x -]> (x = b)) /\ Psi b.
 Proof.
-  exact (n14_1 Phi Psi).
+  exact (n14_1 Phi Psi s).
 Qed.
 
 Theorem n14_11 (E : Predicate 1) (Phi : Prop -> Prop) : 
@@ -130,29 +135,49 @@ Proof.
   now rewrite -> n14_02 in n4_2 at 2.
 Qed.
 
-Theorem n14_111 (Phi Psi : Prop -> Prop) (f : Prop -> Prop -> Prop) :
-  (iota_f2_1 Psi Phi f) <-> (exists b c, 
-    (Phi x <[- x -]> (x = b)) /\ (Psi x <[- x -]> (x = c)) /\  (f b c)).
+Theorem n14_111 (Phi Psi : Prop -> Prop) (f : Prop -> Prop -> Prop) 
+  (s1 s2 : string) :
+  (iota_f2_1 s2 s1 Psi Phi f) <-> (exists b c, 
+    (Phi x <[- x -]> (x = b)) /\ (Psi x <[- x -]> (x = c)) /\ (f b c)).
 Proof.
-  assert (S1 : iota_f2_1 Psi Phi f ↔ iota_f2 Phi Psi f).
+  (* TODO: rededsign S1 *)
+  assert (S1 : iota_f2_1 s2 s1 Psi Phi f ↔ iota_f2 s2 s1 Psi Phi f).
   {
-    pose proof (n4_2 (exists b, (Phi x <[- x -]> (x = b)) 
-      /\ ((exists c, Psi x <[- x -]> (x = c) /\ f b c)))) as n4_2.
-    rewrite <- n14_03 in n4_2 at 2.
-    now rewrite <- n14_04 in n4_2.
+    pose proof (n4_2 (iota_f2_1 s2 s1 Psi Phi f)) as n4_2.
+    rewrite -> n14_04 in n4_2 at 2.
+    rewrite -> (n14_03 s2 s1) in n4_2.
+    simpl in n4_2.
   }
-  (* TODO: design the iota_f & iota_f2 correctly so that we can transform 
-    from f2 to f smoothly
-    Warning: the `b` of `exists b` in the descriptions can also be expanded 
-    into a description
+  (* 
+  Definition iota_f2_1 
+  (s1 s2 : string)
+  (Phi Psi : Prop -> Prop)
+  (f : Prop -> Prop -> Prop) :=
+  iota_f s2 Psi
+    (fun c => iota_f s1 Phi
+      (fun b => f (Iota s1 b) (Iota s2 c))).
   *)
-  assert (S2 : iota_f2_1 Psi Phi f <-> 
-    (iota_f Psi (fun y =>
-      exists b, Phi x <[- x -]> (x = b) /\ f b y))).
+  (* ?????Phi Psi reversed in S1???? *)
+  assert (S2 : iota_f2_1 s2 s1 Psi Phi f <-> 
+    (iota_f s2 Psi (fun c =>
+      exists b, Phi x <[- x -]> (x = b) /\ f b c))).
   {
-    pose proof n14_1 as n14_1.
+    pose proof n14_1 as _n14_1.
     unfold iota_f2 in S1.
-    rewrite <- n14_1 in S1.
+    
+    replace ((λ b : Prop, iota_f s2 Psi (λ c : Prop, f (Iota s1 b) (Iota s2 c))))
+      with ((λ b : Prop, iota_f s2 Psi (λ c : Prop, f b c)))
+    in S1.
+    2: { reflexivity. }
+
+    (* TODO: investigate function extentionality in std *)
+    replace ((λ b : Prop, iota_f s2 Psi (λ c : Prop, f b c)))
+      with (λ c : Prop, ∃ b : Prop,  Phi x<[-x-]>x = b ∧ f b c)
+    in S1.
+    2: {
+      rewrite -> (n14_1).
+    }
+    
   }
   pose proof n14_1 as n14_1.
   pose proof n11_55 as n11_55.
