64cee35f99082deb690762f725b5ec36b56d8891
MudroadWhite <53061787+MudroadWhite@users.noreply.github.com>
2026-03-18 16:21:19 +0800

    minor: progress

--- pm/ch12.v
+++ pm/ch12.v
@@ -15,6 +15,7 @@ into rocq
 (* 
 Critics: f and F are both of same Order, and they should be actually one untyped while the other
 type of "predicate to x"
+TODO: define a temporarily notion of "untyped", and make a clear use to `!`; fill in documents
 *)
 Definition n12_1 (φ : Prop → Prop) : 
   ∃ f : (Order 1), (φ x) <[- x -]> ((fun (F : Order 1) =>
